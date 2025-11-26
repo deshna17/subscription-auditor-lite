cea79fd63d672da516f9952d34b6ffa6c6d0f410
Deshna Jain <87945478+deshna17@users.noreply.github.com>
2025-11-26 19:08:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import ChatUI from "./components/ChatUI";
 
 export default function Home() {
   return (
-    <main>
-      <div className="chat-shell">
-        <ChatUI />
-      </div>
+    <main className="page">
+      <ChatUI />
     </main>
   );
 }
