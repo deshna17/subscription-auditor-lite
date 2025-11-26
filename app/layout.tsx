260ea3d7bb10169e7d7a9522d71c4181ff60b95c
Deshna Jain <87945478+deshna17@users.noreply.github.com>
2025-11-26 17:38:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Subscription Auditor (Lite)",
+  description: "Helps you understand your subscriptions & find cheaper alternatives."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
